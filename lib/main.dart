a29142d302a63723475b2b1894d7590b7d54f8ab
yabeye <yeabsera.work@gmail.com>
2023-03-01 19:48:28 +0300

    Added searching text filed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,9 @@ class WhalesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Whales App',
-      theme: ThemeData(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       home: const Main(),
@@ -26,6 +29,7 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -39,6 +43,68 @@ class Main extends StatelessWidget {
           ],
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: deviceSize.width * 1,
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Search Github username",
+                      prefixIcon: Icon(Icons.search),
+                      suffix: TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.blue,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      )),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _sizedContainer(
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(300.0),
+                      child: CachedNetworkImage(
+                        progressIndicatorBuilder: (context, url, progress) =>
+                            Center(
+                          child: CircularProgressIndicator(
+                            value: progress.progress,
+                          ),
+                        ),
+                        imageUrl:
+                            'https://avatars.githubusercontent.com/u/88554326?v=4',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+Widget _sizedContainer(Widget child) {
+  return SizedBox(
+    width: 80.0,
+    height: 80.0,
+    child: Center(child: child),
+  );
 }
