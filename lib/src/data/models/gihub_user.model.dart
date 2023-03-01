61e973e74cdc7dfe4c6e6df66717c79d7a38ae04
yabeye <yeabsera.work@gmail.com>
2023-03-01 20:34:13 +0300

    Created a github user model.

---
+++ lib/src/data/models/gihub_user.model.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+GithubUser githubUserFromJson(String str) =>
+    GithubUser.fromJson(json.decode(str));
+
+String githubUserToJson(GithubUser data) => json.encode(data.toJson());
+
+class GithubUser {
+  GithubUser({
+    this.login,
+    required this.id,
+    this.avatarUrl,
+    this.name,
+    this.location,
+    this.email,
+    this.bio,
+    this.twitterUsername,
+    this.publicRepos,
+    required this.followers,
+    required this.following,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  String? login;
+  int id;
+  String? avatarUrl;
+  String? name;
+  String? location;
+  String? email;
+  String? bio;
+  String? twitterUsername;
+  int? publicRepos;
+  int followers;
+  int following;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  factory GithubUser.fromJson(Map<String, dynamic> json) => GithubUser(
+        login: json["login"],
+        id: json["id"],
+        avatarUrl: json["avatar_url"],
+        name: json["name"],
+        location: json["location"],
+        email: json["email"],
+        bio: json["bio"],
+        twitterUsername: json["twitter_username"],
+        publicRepos: json["public_repos"],
+        followers: json["followers"],
+        following: json["following"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "login": login,
+        "id": id,
+        "avatar_url": avatarUrl,
+        "name": name,
+        "location": location,
+        "email": email,
+        "bio": bio,
+        "twitter_username": twitterUsername,
+        "public_repos": publicRepos,
+        "followers": followers,
+        "following": following,
+        "created_at": createdAt.toIso8601String(),
+        "updated_at": updatedAt.toIso8601String(),
+      };
+}
